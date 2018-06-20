0d87be3665e1c9c8bb7fd59c42f93a872b9a117d
ARJUN K S <arjun@ks.ks>
2018-06-20 22:45:57 +0530

    Version 1.0

--- script.js
+++ script.js
@@ -93,9 +93,34 @@ function validatesignup()
   else
     {
       document.getElementById("gender").innerHTML='Please select your gender';
-      signup4=false;}
+      signup4=false;
+  }
 
-  if(signup1 && signup2 && signup3 && signup4)
+  signup5=true;
+  document.getElementById('vdob').innerHTML='';
+  if(document.getElementById('dob').value=='')
+  {
+  	
+  	document.getElementById('vdob').innerHTML='Please enter a DOB';
+  	signup5=false;
+  }
+
+
+
+  signup6=true;
+  document.getElementById('vcountry').innerHTML='';
+  if(document.getElementById('country').value=='Select your Country')
+  {
+  	
+  	document.getElementById('vcountry').innerHTML='Please select a country';
+  	signup6=false;
+  }
+
+
+
+
+
+  if(signup1 && signup2 && signup3 && signup4 && signup5 && signup6)
   {
         
     
@@ -111,6 +136,7 @@ function validatesignup()
     if(typeof(Storage) !== "undefined") {
         if (localStorage.clickcount) {
             localStorage.clickcount = 'Last entered SI No: is '+sino;
+
             localStorage.sino=sino+1;
             localStorage.resultGENDE = getgender;
             localStorage.resultname = getName;
@@ -118,6 +144,7 @@ function validatesignup()
             localStorage.resultPassword = getPasword;
             localStorage.resultDOB = getDOB;
             localStorage.resultCountry = getcountry;
+
             var table = document.getElementById("myTable");
             var row = table.insertRow(1);
             var cell0 = row.insertCell(0);
@@ -135,13 +162,21 @@ function validatesignup()
             cell5.innerHTML = getDOB;
             cell6.innerHTML = getcountry;
         } else {
-            localStorage.clickcount = 'empty';
+            localStorage.clickcount = 'Table empty';
 
         }
         document.getElementById("result").innerHTML = localStorage.clickcount ;
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
+
+    //CLEAR FORM
+    document.getElementById("Mr.").checked = document.getElementById("Ms.").checked = document.getElementById("Mx.").checked=false;
+    document.getElementById("name").value='';
+    document.getElementById("email").value='';
+    document.getElementById("password").value='';
+    document.getElementById("dob").value='';
+    document.getElementById("country").value='Select your Country';
   }
 
 }
@@ -150,7 +185,7 @@ function validatesignup()
 $(function(){
    var country_arr = new Array("Select your Country","Afghanistan", "Albania", "Algeria", "American Samoa", "Angola", "Anguilla", "Antartica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Ashmore and Cartier Island", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Clipperton Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czeck Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Europa Island", "Falkland Islands (Islas Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern and Antarctic Lands", "Gabon", "Gambia, The", "Gaza Strip", "Georgia", "Germany", "Ghana", "Gibraltar", "Glorioso Islands", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Holy See (Vatican City)", "Honduras", "Hong Kong", "Howland Island", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Ireland, Northern", "Israel", "Italy", "Jamaica", "Jan Mayen", "Japan", "Jarvis Island", "Jersey", "Johnston Atoll", "Jordan", "Juan de Nova Island", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Man, Isle of", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Midway Islands", "Moldova", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcaim Islands", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romainia", "Russia", "Rwanda", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Scotland", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and South Sandwich Islands", "Spain", "Spratly Islands", "Sri Lanka", "Sudan", "Suriname", "Svalbard", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Tobago", "Toga", "Tokelau", "Tonga", "Trinidad", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "USA", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands", "Wales", "Wallis and Futuna", "West Bank", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
    var $select = $(".countrylist");
-    for (i=0;i<=country_arr.length;i++){
+    for (i=0;i<country_arr.length;i++){
         $select.append($('<option></option>').val(country_arr[i]).html(country_arr[i]))
     }
 });
@@ -159,7 +194,7 @@ function cleardata()
 {
   var i=0,k=0;
   sino=0;
-  localStorage.clickcount='empty';
+  localStorage.clickcount='Table empty';
   document.getElementById("result").innerHTML = localStorage.clickcount ;
   k = document.getElementById("myTable").rows.length; 
   for(j=0;j<k-2;j++)
